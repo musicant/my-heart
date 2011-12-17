83949ee4ed15eb263c27a422a3aafc7bd1edbd84
Anton Popov <musicant@mail.localhost.com>
2011-12-17 14:10:56 +0200

    мердж

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,13 +11,34 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        $request = $this->getRequest();
+        $VKParams = new Zend_Session_Namespace('testSpace');
+        $VKParams->requestParams = $request->getParams();
     }
 
     public function friendsAction()
     {
         // action body
+        $VKParams = new Zend_Session_Namespace('testSpace');
+        $VK = new Application_Model_VK();
+        $currentUserId = $VKParams->requestParams['user_id'];
+
+        $this->view->currentUserId = $currentUserId;
+        $this->view->friendsData = $VK->getFriendsList($currentUserId);
     }
 
+
+    public function postCardAction()
+    {
+        // action body
+        $VKParams = new Zend_Session_Namespace('testSpace');
+        $currentUserId = $VKParams->requestParams['user_id'];
+
+        $request = $this->getRequest();
+        $myFriendId = $request->getParam('my-friend');
+        $this->view->myFriendId = $myFriendId;
+        $this->view->currentUserId = $currentUserId;
+    }
 
 }
 
