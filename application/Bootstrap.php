a34b621150b62bebd2cb2b6b98f752381a1f9588
Anton Popov <musicant@mail.localhost.com>
2011-12-24 17:04:06 +0200

    дід мороз. нові малюнки

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -12,14 +12,14 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $VK_Config['api_secret'] = "ubNrwsizrFN5LJmWhAeD";
         $VK_Config['api_url'] = "http://api.vkontakte.ru/api.php";
 
-        define("SANTA_ID",'5701489');
-        define("SANTA_ALBUM_ID",'145745462');
+        define("SANTA_ID",'156671327');
+        define("SANTA_ALBUM_ID",'148974122');
 
 
         //configuration for "wallposter"
-        define('VKEMAIL', 'djas-best@mail.ru');				// vk email/login
-        define('VKPWD', 'AwdarkIbUd');			// vk password
-        define('VKPHONE', '2321');				// FOURTH (4!!!) LAST digits from phone
+        define('VKEMAIL', '+380935006230');				// vk email/login
+        define('VKPWD', 'happynewyear12');			// vk password
+        define('VKPHONE', '6230');				// FOURTH (4!!!) LAST digits from phone
 
         // Other config
         define('SLEEPTIME', 2);					// in seconds; +rand(1, 4) if this var != 0
